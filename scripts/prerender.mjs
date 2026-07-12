156c2b82f647582b84110725898adae80a8db09a
ireflux <mapleblue2007@gmail.com>
2026-07-13 01:01:49 +0800

    fix path

--- scripts/prerender.mjs
+++ scripts/prerender.mjs
@@ -1,8 +1,9 @@
 import { render, routePaths } from '../dist/entry-server.js'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname).replace(/^\//, '')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // Read the built index.html as template
 const templatePath = path.resolve(__dirname, '..', 'dist', 'index.html')
